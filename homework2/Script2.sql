a1acd9afd63d116210ce02bc48a0b8ff82f60681
VPesherov <vyacheslav.pesherov@yandex.ru>
2023-07-03 15:38:13 +0700

    editing script and scheme

--- homework2/Script2.sql
+++ homework2/Script2.sql
@@ -1,4 +1,4 @@
---employee_communications
+
 CREATE TABLE IF NOT EXISTS employees (
 	employee_id SERIAL primary key not null,
 	name varchar(100) not null
@@ -13,6 +13,6 @@ CREATE TABLE IF NOT EXISTS deparments (
 CREATE TABLE IF NOT EXISTS deparment_staff (
 	employee_id integer not null references employees(employee_id),
 	department_id integer not null references deparments(department_id),
-	boss integer null references employees(employee_id),
+	boss_id integer null references employees(employee_id),
 	constraint pk1 primary key (employee_id, department_id)
 );
